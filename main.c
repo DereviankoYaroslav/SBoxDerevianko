eb3d72b455237cd54c07e8aa7fbe2f223557b401
olenaderevianko <dereviankoolenav@gmail.com>
2021-08-12 16:22:08 +0300

    +fixed AC

--- main.c
+++ main.c
@@ -88,8 +88,6 @@ int ACOfSBox(int *sbox, int size, int count);
 
 int *simulatedAnnealing(const int *sbox, int size, int count);
 
-int testNL(int *sbox, int size, int count);
-
 int NLOfLinearCombinations(const int *arr, int size, int count);
 
 int deltaUniformity(const int *arr, int size, int count);
@@ -123,6 +121,10 @@ int algebraicImmunity(const int *sbox, int size, int count);
 void bubble_sort(int *data, int size);
 
 int *WHTSpectrumForLinearComb(const int *arr, int size, int count);
+
+int *ACForLinearComb(const int *arr, int size, int count);
+
+int *DegForLinearComb(const int *arr, int size, int count);
 
 int linearRedundancy(int *sbox, int size, int count);
 
@@ -663,9 +665,67 @@ int main(int args, char **argv) {
 
     int ar2[] = {30, 43, 217, 136, 160, 233, 123, 179, 111, 93, 184, 114, 241, 4, 149, 107, 251, 21, 226, 131, 99, 25, 6, 48, 180, 186, 61, 7, 134, 191, 249, 109, 177, 23, 198, 105, 78, 183, 141, 89, 253, 68, 192, 90, 60, 49, 39, 240, 101, 225, 98, 63, 210, 77, 207, 120, 124, 129, 147, 162, 176, 11, 250, 59, 148, 37, 199, 9, 92, 34, 248, 53, 24, 62, 151, 238, 138, 82, 224, 26, 178, 44, 29, 206, 95, 132, 18, 128, 33, 185, 212, 236, 223, 169, 71, 170, 254, 219, 188, 156, 14, 182, 211, 31, 86, 166, 234, 244, 171, 116, 3, 252, 1, 193, 140, 13, 133, 126, 110, 35, 103, 139, 201, 173, 66, 167, 209, 158, 117, 70, 222, 119, 175, 174, 200, 81, 17, 146, 46, 232, 67, 87, 214, 230, 51, 181, 32, 115, 135, 55, 72, 157, 122, 45, 22, 10, 79, 15, 50, 187, 102, 83, 41, 216, 194, 229, 88, 12, 227, 57, 125, 19, 54, 190, 127, 94, 220, 197, 144, 242, 85, 208, 73, 189, 163, 16, 152, 243, 239, 96, 106, 36, 64, 245, 161, 47, 97, 150, 113, 213, 56, 130, 100, 27, 165, 215, 195, 143, 20, 58, 108, 52, 74, 218, 69, 2, 153, 145, 5, 38, 205, 203, 65, 235, 204, 247, 172, 80, 121, 28, 202, 246, 142, 228, 0, 91, 8, 104, 221, 155, 255, 75, 137, 40, 168, 118, 154, 196, 42, 76, 159, 84, 164, 231, 112, 237};
 
-    int lr = linearRedundancy(arr,8,3);
 
-    printf("LR = %d ", lr);
+    //int lr = linearRedundancy(aesSbox,256,8);
+
+    int arr1[] = {0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
+                  1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+                  0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+                  1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0,
+                  1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0,
+                  0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1,
+                  1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1};
+
+    int arr2[] = {1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0,
+                  1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+                  1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1,
+                  1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+                  1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0,
+                  0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0,
+                  1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,};
+
+    int *whtar1 = HadamardCoefficients(arr1,256,8);
+    bubble_sort(whtar1,256);
+
+    for (int i = 0; i < 256; ++i){
+        printf("%d ", whtar1[i]);
+    }
+    printf("\n");
+
+    int AD = algebraicDeg(arr1,256,8);
+    printf("AD = %d ", AD);
+    printf("\n");
+
+    int *ac1 = autoCorrelation(arr1, 256, 8);
+    int AC = autoCorrelationMax(ac1,256);
+    printf("AC = %d ", AC);
+    printf("\n");
+
+    int *whtar2 = HadamardCoefficients(arr2,256,8);
+    bubble_sort(whtar2,256);
+
+    for (int i = 0; i < 256; ++i){
+        printf("%d ", whtar2[i]);
+    }
+    printf("\n");
+
+    int AD2 = algebraicDeg(arr2,256,8);
+    printf("AD2 = %d ", AD2);
+    printf("\n");
+
+    int *ac2 = autoCorrelation(arr2, 256, 8);
+    int AC2 = autoCorrelationMax(ac2,256);
+    printf("AC2 = %d ", AC2);
+    printf("\n");
+
+    int f[] = {0,0,1,0,1,0,1,1};
+    int *ac3 = autoCorrelation(f, 8, 3);
+    int AC3 = autoCorrelationMax(ac3,3);
+    printf("AC3 = %d ", AC3);
+    printf("\n");
+
+
+    //printf("LR = %d ", lr);
 
     //int nl = NLOfSBox(ar2,256,8);
 
@@ -1075,13 +1135,21 @@ int HadamardNLinearity(int max, int count) {
 //Функція обчислення автокореляційної функції
 
 int *autoCorrelation(int *func, int size, int count) {
+    int temp = 0;
     int *acFunc = calloc(size, sizeof(int));
     int *polFunc = toPolarTable(func, size);
+    int *polFunc2 = calloc(size, sizeof(int));
+    for (int i =0, k = size-1; i<size, k>=0; ++i, k--) {
+        polFunc2[i] = polFunc[k];
+        //printf("\npf2= %d", polFunc2[i]);
+    }
     acFunc[0] = raiseToPower(2, count);
     for (int i = 1; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            int temp = polFunc[j] * polFunc[j ^ i];
-            //printf("j^i = %d", j^i);
+            temp = polFunc2[j] * polFunc2[j ^ i];
+            //printf("\nj = %d", j);
+            //printf("\nj^i = %d", j^i);
+            //printf("\ntemp= %d", temp);
             acFunc[i] = acFunc[i] + temp;
         }
         //printf("ac i = %d", acFunc[i]);
@@ -2539,7 +2607,22 @@ int *WHTSpectrumForLinearComb(const int *arr, int size, int count) {
             //printf("%d ", fxarr[q]);
             result[i*size+q] = fxarr[q];
         }
-        int *ar = calloc(size, sizeof(int));
+    }
+    free(fxarr);
+    free(temp);
+    return result;
+}
+
+int *ACForLinearComb(const int *arr, int size, int count) {
+    int *result = calloc(size-1, sizeof(int));
+    int *temp = calloc(size, sizeof(int));
+    int *ar = calloc(size, sizeof(int));
+    for (int i = 0; i < size - 1; ++i) {
+        //printf("\nCombination %d", i + 1);
+        for (int j = 0; j < size; ++j) {
+            temp[j] = arr[i * size + j];
+            //printf("%d ", temp[j]);
+        }
         ar = autoCorrelation(temp, size, count);
 
         /*printf("\nAUTO CORRELATING FUNCTION");
@@ -2548,17 +2631,32 @@ int *WHTSpectrumForLinearComb(const int *arr, int size, int count) {
             printf("%d ", ar[w]);
         }
         printf("\n");*/
-
         int AC = autoCorrelationMax(ar, size);
         //printf("\nAUTO CORRELATION = %d", AC);
         //printf("\n");
         //maxAC[i] = AC;
+       result[i] = AC;
+    }
+    free(ar);
+    free(temp);
+    return result;
+}
+
+int *DegForLinearComb(const int *arr, int size, int count) {
+    int *result = calloc((size-1), sizeof(int));
+    int *temp = calloc(size, sizeof(int));
+    for (int i = 0; i < size - 1; ++i) {
+        //printf("\nCombination %d", i + 1);
+        for (int j = 0; j < size; ++j) {
+            temp[j] = arr[i * size + j];
+            //printf("%d ", temp[j]);
+        }
         int dec = algebraicDeg(temp, size, count);
-        printf("\nALGEBRAIC DEGREE = %d ", dec);
+        //printf("\nALGEBRAIC DEGREE = %d ", dec);
         //printf("\n");
         //minDEC[i] = dec;
+        result[i] = dec;
     }
-    free(fxarr);
     free(temp);
     return result;
 }
@@ -2567,11 +2665,13 @@ int linearRedundancy(int *sbox, int size, int count){
     int result;
     int *ar1 = SBoxToBooleanFunc(sbox,size,count);
     int *ar2 = linearCombinations(ar1, size, count);
-    /*int ar3[size-1][size];
-    for (int i = 0; i < size-1; ++i){
+    int *ar4 = calloc((size-1),sizeof(int));
+    int *ar5 = calloc((size-1),sizeof(int));
+    int ar33[size-1][size];
+    /*for (int i = 0; i < size-1; ++i){
         for(int j = 0; j<size; ++j){
-            ar3[i][j] = ar2[i*size+j];
-            printf("%d ", ar3[i][j]);
+            ar33[i][j] = ar2[i*size+j];
+            printf("%d, ", ar33[i][j]);
         }
         printf("\n");
     }*/
@@ -2579,18 +2679,26 @@ int linearRedundancy(int *sbox, int size, int count){
     /*for (int i = 0; i < size*(size-1); ++i){
         printf("%d ", ar3[i]);
     }*/
+    ar4 = ACForLinearComb(ar2,size,count);
+    ar5 = DegForLinearComb(ar2,size,count);
     int sp [size-1][size];
     for (int i = 0; i < size - 1; ++i) {
         for (int q = 0; q < size; ++q) {
             sp[i][q] = ar3[i*size+q];
         }
     }
-    for (int i = 0; i < size-1; ++i){
+    /*for (int i = 0; i < size-1; ++i){
         for(int j = 0; j<size; ++j){
             printf("%d ", sp[i][j]);
         }
         printf("\n");
     }
+    for (int i = 0; i < size-1; ++i){
+        printf("%d ", ar4[i]);
+    }
+    for (int i = 0; i < size-1; ++i){
+        printf("%d ", ar5[i]);
+    }*/
     int innerCounter = 0;
     int OuterCounter = 0;
     int finalCounter = 0;
@@ -2614,11 +2722,15 @@ int linearRedundancy(int *sbox, int size, int count){
                 printf("%d ", sp[h][j]);
             }*/
             if (innerCounter == size){
-                //printf("\nSTRINGS ARE EQUAL");
-                OuterCounter++;
-                for (int j = 0; j < size; ++j){
-                    sp[h][j] = -99999;
-                }
+                //if (i!=h) {
+                    //if (ar4[i]==ar4[h] && ar5[i] == ar5[h]) {
+                        //printf("\nSTRINGS ARE EQUAL");
+                        OuterCounter++;
+                        for (int j = 0; j < size; ++j) {
+                            sp[h][j] = -99999;
+                        }
+                    //}
+                //}
             }
             innerCounter = 0;
         }
